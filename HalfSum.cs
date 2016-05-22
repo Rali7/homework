ad8c2c0d12672113965d8a085575db179844f99e
Rali7 <ralitsa.kolibarova@yahoo.com>
2016-05-22 09:35:21 +0300

    Add files via upload

---
+++ HalfSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class HalfSum
+{
+	public static void Main (string[] args)
+	{
+		int sumFirst = 0;
+		int sumSecond = 0;
+		int number = int.Parse (Console.ReadLine ());
+		for (int i = 0; i < number; i++) {
+
+			int halfSequenceFirst = int.Parse (Console.ReadLine());
+			sumFirst += halfSequenceFirst;
+		}
+		for (int i = 0; i < number; i++) {
+
+			int halfSumSecond = int.Parse (Console.ReadLine());
+			sumSecond += halfSumSecond;
+		}
+		if(sumFirst == sumSecond)
+			Console.WriteLine ("Yes, sum=" + sumFirst);
+		else
+			Console.WriteLine ("No, diff=" + Math.Abs(sumFirst - sumSecond));
+	}
+}
